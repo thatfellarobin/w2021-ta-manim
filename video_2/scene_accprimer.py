64773631b3cc22d9c1644a5f4885aaf1a2123003
RobinL <Robin.Liu.831@gmail.com>
2021-01-23 17:08:33 -0500

    progress on q1 and minor accprimer edits

--- video_2/scene_accprimer.py
+++ video_2/scene_accprimer.py
@@ -14,20 +14,14 @@ class AccPrimer(Scene):
         self.position = particle.get_center()
         self.oldposition = particle.get_center()
         self.vel = 0
+        self.oldvel = 0
+        self.acc_tan = 0
 
         self.max_tllratio=0.5 # Max tip length to length ratio for arrows
         self.max_swlratio=10 # Max stroke width to length ratio
         self.arrow_scale=0.5 # Scaling the arrows cause they be beeg otherwise
 
         # Arrows
-        acc_vector = Arrow(
-            start=particle.get_center(),
-            end=ORIGIN,
-            buff=0,
-            max_tip_length_to_length_ratio=self.max_tllratio,
-            max_stroke_width_to_length_ratio=self.max_swlratio,
-            color=RED
-        )
         vel_vector = Arrow(
             start=particle.get_center(),
             end=particle.get_center()+UP,
@@ -36,11 +30,28 @@ class AccPrimer(Scene):
             max_stroke_width_to_length_ratio=self.max_swlratio,
             color=BLUE
         )
+        acc_vector = Arrow(
+            start=particle.get_center(),
+            end=ORIGIN,
+            buff=0,
+            max_tip_length_to_length_ratio=self.max_tllratio,
+            max_stroke_width_to_length_ratio=self.max_swlratio,
+            color=RED
+        )
+        acc_tan_vector = Arrow(
+            start=particle.get_center(),
+            end=particle.get_center()+UP,
+            buff=0,
+            max_tip_length_to_length_ratio=self.max_tllratio,
+            max_stroke_width_to_length_ratio=self.max_swlratio,
+            color=MAROON
+        )
 
         # Text and annotations
         title = Tex('\\textbf{Particle velocity and centripetal accelerations}').shift(3*UP)
-        vel_label = MathTex('v(t)', color=BLUE).shift(4.25*LEFT + 0.5*UP)
-        acc_label = MathTex('a_c(t)', '=', '\\frac{v^2}{\\rho}', color=RED).align_to(vel_label, LEFT).shift(0.5*DOWN)
+        vel_label = MathTex('v(t)', color=BLUE).shift(4.25*LEFT + 0.6*UP)
+        acc_label = MathTex('a_c(t)', '=', '\\frac{v^2}{\\rho}', color=RED).next_to(vel_label, DOWN, aligned_edge=LEFT)
+        acc_tan_label = MathTex('a_t(t)', color=MAROON).next_to(acc_label, DOWN, aligned_edge=LEFT)
         equation_explain = Tex('$\\rho$ is the', '\\textit{instantaneous}', 'path radius').shift(5.5*RIGHT + 0.25*UP).scale(0.9)
         equation_explain[1].set_color(YELLOW).shift(0.1*RIGHT)
         equation_explain[2].shift(0.5*DOWN).align_to(equation_explain[0], LEFT)
@@ -49,11 +60,18 @@ class AccPrimer(Scene):
 
         # Updater functions
         def update_vel(vel, dt):
-            # Update velocity
+            # Update velocity and tangential acceleration
             self.position = particle.get_center()
             if abs(dt) > 0:
                 self.vel = (self.position - self.oldposition) / dt
+            if abs(dt) > 0 and np.linalg.norm(self.vel) > 0:
+                self.acc_tan = \
+                    (self.vel / np.linalg.norm(self.vel)) * \
+                    ((np.linalg.norm(self.vel) - np.linalg.norm(self.oldvel)) / dt)
+            else:
+                self.acc_tan = 0
             self.oldposition = self.position
+            self.oldvel = self.vel
 
             # New arrow
             new_vel_vector = Arrow(
@@ -65,7 +83,6 @@ class AccPrimer(Scene):
                 color=BLUE
             )
             vel.become(new_vel_vector)
-
         def update_acc(acc):
             base = particle.get_center()
             new_acc_vector_math = self.arrow_scale * (-base / np.linalg.norm(base)) * (np.linalg.norm(self.vel)**2 / np.linalg.norm(base)) # Direction * magnitude
@@ -78,13 +95,25 @@ class AccPrimer(Scene):
                 color=RED
             )
             acc.become(new_acc_vector)
+        def update_acc_tan(acc):
+            base = particle.get_center()
+            new_acc_vector = Arrow(
+                start=base,
+                end=base + self.arrow_scale * self.acc_tan,
+                buff=0,
+                max_tip_length_to_length_ratio=self.max_tllratio,
+                max_stroke_width_to_length_ratio=self.max_swlratio,
+                color=MAROON
+            )
+            acc.become(new_acc_vector)
 
         # Attach updaters
         vel_vector.add_updater(update_vel)
         acc_vector.add_updater(update_acc)
+        # acc_tan_vector.add_updater(update_acc_tan)
 
         # Run animations
-        self.add(title, origin, path, acc_vector, vel_vector, particle, vel_label, acc_label[0])
+        self.add(title, origin, path, acc_vector, vel_vector, particle, vel_label, acc_label[0], acc_tan_label)
         self.wait(0.5)
         self.play(Rotating(particle, radians=0.8*TAU, about_point=ORIGIN, rate_func=smooth),  run_time=5)
         self.wait(0.1)
--- video_2/scene_q1.py
+++ video_2/scene_q1.py
@@ -2,93 +2,118 @@ from manim import *
 import numpy as np
 
 
-class P1(Scene, MovingCameraScene):
-    # CONFIG = { # Originally thought I'd use GraphSchene
-    #     'x_min': 0,
-    #     'x_max': 20,
-    #     'x_labeled_nums': np.arange(0, 25, 5),
-    #     'x_axis_label': "$t$",
-    #     # 'x_axis_width': 7,
-    #     'x_axis_config': {"tick_frequency": 2.5},
-    #     'y_min': 0,
-    #     'y_max': 8,
-    #     'y_labeled_nums': np.arange(0, 10, 2),
-    #     'y_axis_label': None,
-    #     # 'y_axis_height': 5.5
-    #     # 'y_axis_config': {"tick_frequency": 2.5}
-    # }
-
-    def setup(self):
-        GraphScene.setup(self)
-        MovingCameraScene.setup(self)
+class P1(Scene):
 
     def construct(self):
-        a = MathTex("a(t)", "=", "\\frac{dv(t)}{dt}", "=", "0.015t^2", color=RED)
-        v = MathTex("v(t)", "=", "\\int a(t)\\,dt", "=", "0.005t^3 + v_0", color=BLUE)
-        s = MathTex("s(t)", "=", "\\int v(t)\\,dt", "=", "0.00125t^4 + v_0t + s_0", color=GREEN)
+        # Scene imagery
+        diagram_scale = 0.04
+        d = 100 * diagram_scale
+        rho = 80 * diagram_scale
+        path_1 = Line(start=ORIGIN, end=d*LEFT, color=GREY).shift(2.5*UP + 2*RIGHT)
+        path_2 = Arc(radius=rho, start_angle=TAU/4, angle=TAU/4, color=GREY).next_to(path_1, direction=LEFT, buff=0, aligned_edge=UP)
+        car = Dot(point=ORIGIN, color=YELLOW).move_to(path_1.get_start())
+        path = path_1.copy()
+        path.append_vectorized_mobject(path_2)
+
+        # Annotations
+        starting_line = Line(start=ORIGIN, end=0.4*DOWN, color=GREEN).next_to(path_1.get_start(), DOWN)
+        s_arrow = Arrow(start=path.get_start(), end=path.get_start(), buff=0, color=GREEN)
+        s_arrow_label = MathTex('s', color=GREEN).next_to(starting_line, RIGHT)
+
+        # time functions
+        a = MathTex("a_t(t)", "=", "0.015t^2", color=RED).scale(0.9).shift(1.5*UP)
+        v = MathTex("v(t)", "=", "\\int a_t(t)\\,dt", "=", "0.005t^3 + v_0", color=BLUE).scale(0.9).next_to(a, DOWN, aligned_edge=LEFT)
+        s = MathTex("s(t)", "=", "\\int v(t)\\,dt", "=", "0.00125t^4 + v_0t + s_0", color=GREEN).scale(0.9).next_to(v, DOWN, aligned_edge=LEFT)
+        # time functions after solving init conditions
+        v_final = MathTex("v(t)", "=", "0.005t^3", color=BLUE).scale(0.9)
+        v_final.shift(v[0].get_center() - v_final[0].get_center())
+        s_final = MathTex("s(t)", "=", "0.00125t^4", color=GREEN).scale(0.9)
+        s_final.shift(s[0].get_center() - s_final[0].get_center())
+        # time functions after substitution
+        s_subbed = MathTex("s(18)", "=", "131.22\\,\\mathrm{m}", color=GREEN).scale(0.9).align_to(s_final, direction=LEFT, alignment_vect=UP)
+        v_subbed = MathTex("v(t)", "=", "29.16\\,\\mathrm{m/s^2}", color=BLUE).scale(0.9).align_to(v_final, direction=LEFT, alignment_vect=UP)
 
         # Initial conditions
-        initcond_prose = Tex("The automobile is \\textbf{originally} at \\textbf{rest} at $s=0$", color=YELLOW)
-        initcond = Tex("$s(0)=0$, $v(0)=0$", color=YELLOW)
+        initcond_prose = Tex('"Originally at rest at $s=0$"', color=YELLOW).scale(0.8).next_to(s_final, DOWN, aligned_edge=LEFT, buff=2*DEFAULT_MOBJECT_TO_MOBJECT_BUFFER)
+        initcond = Tex("$s(0)=0$, $v(0)=0$", color=YELLOW).scale(0.8).next_to(initcond_prose, DOWN, aligned_edge=LEFT)
+
+
+        # Updater Functions
+        def s_arrow_updater(s_arrow):
+            # Warning: only works in the straight part of the path.
+            start = starting_line.get_center()
+            end = (car.get_center() - path.get_start()) + start
+            # New arrow
+            new_s_arrow = Arrow(
+                start=start,
+                end=end,
+                buff=0,
+                max_stroke_width_to_length_ratio=999,
+                max_tip_length_to_length_ratio=1,
+                color=GREEN
+            )
+            s_arrow.become(new_s_arrow)
+
+
+
+        # set up
+        self.add(path, car)
+        self.wait()
+
+        # Explain diff parts of the scene
+        self.play(Flash(car))
+        self.play(Flash(car))
+        self.wait()
+        self.play(ShowPassingFlash(path.copy().set_color(YELLOW)))
+        self.play(ShowPassingFlash(path.copy().set_color(YELLOW)))
+        self.wait()
+
+        # Explain how s is measured
+        self.play(
+            FadeIn(starting_line),
+            FadeIn(s_arrow_label)
+        )
+        s_arrow.add_updater(s_arrow_updater)
+        self.add(s_arrow)
+        self.play(MoveAlongPath(car, path_1), run_time=4, rate_func=there_and_back)
+        s_arrow.clear_updaters()
+        self.remove(s_arrow)
+        self.wait(0.5)
+        self.play(
+            FadeOut(starting_line),
+            FadeOut(s_arrow_label)
+        )
+        self.wait()
+
+        # Go through math for time functions
+        self.play(Write(a))
+        self.wait()
+        self.play(Write(v))
+        self.wait()
+        self.play(Write(s))
+        self.wait()
+
+        # Go through initial conditions
         self.play(Write(initcond_prose))
         self.wait()
-        self.play(Write(initcond_prose))
+        self.play(Write(initcond))
         self.wait()
-        # There should be some animations here showing `s` and `v` getting modified.
+        self.play(
+            FadeOut(v[2]),
+            FadeOut(v[3]),
+            FadeOut(s[2]),
+            FadeOut(s[3])
+        )
+        self.play(
+            ReplacementTransform(v[0], v_final[0]),
+            ReplacementTransform(v[1], v_final[1]),
+            ReplacementTransform(v[4], v_final[2]),
+            ReplacementTransform(s[0], s_final[0]),
+            ReplacementTransform(s[1], s_final[1]),
+            ReplacementTransform(s[4], s_final[2])
+        )
+        self.wait()
 
-        # # Indicate turnaround point
-        # turnaround_circle = Circle(color=YELLOW, radius=0.15).move_to(self.coords_to_point(2, 0))
-        # self.play(ShowCreation(turnaround_circle))
-        # self.wait()
-        # self.play(turnaround_circle.animate.shift(self.coords_to_point(2, 4) - self.coords_to_point(2, 0)))
-        # self.wait()
+        # Figure out where it is at t1
 
-        # # Expression to solve for the turnaround time
-        # time_t2 = MathTex("t_2", color=PURPLE).move_to(self.coords_to_point(2, 6.5))
-        # turnaround_expression = MathTex("v(t_2)=0\\Rightarrow t_2=2\\,\\mathrm{s}").move_to(self.coords_to_point(2, 9))
-        # self.play(
-        #     Write(time_t2),
-        #     Write(turnaround_expression)
-        # )
-        # self.wait()
-        # self.play(
-        #     FadeOut(time_t2),
-        #     FadeOut(turnaround_expression)
-        # )
 
-        # # Show the calculation of total distance on the graph
-        # turnaround_line = self.get_vertical_line_to_graph(2, pos_graph, color=YELLOW)
-        # self.play(
-        #     FadeOut(vel_graph),
-        #     FadeOut(vel_graph_label),
-        #     FadeOut(acc_graph),
-        #     FadeOut(acc_graph_label),
-        #     ReplacementTransform(turnaround_circle, turnaround_line)
-        # )
-        # self.wait()
-        # totaldist = MathTex(
-        #     "d_{tot}=", "|s(2)-s(0)|", "+", "|s(3)-s(2)|"
-        #     ).shift(0.75 * DOWN + 0.75 * RIGHT)
-        # self.play(Write(totaldist))
-        # dist1_box = SurroundingRectangle(totaldist[1], buff=.1)
-        # dist2_box = SurroundingRectangle(totaldist[3], buff=.1)
-        # self.wait()
-        # self.play(ShowCreation(dist1_box))
-        # self.wait()
-        # self.play(ReplacementTransform(dist1_box, dist2_box))
-        # self.wait()
-        # self.play(FadeOut(dist2_box))
-        # distresult_intermediate = MathTex(
-        #     "d_{tot}=", "|4-0|", "+", "|0-4|"
-        #     ).shift(0.75 * DOWN).align_to(totaldist, direction=LEFT)
-        # distresult_final = MathTex(
-        #     "d_{tot}=", "8\\,\\mathrm{m}"
-        #     ).shift(0.75 * DOWN).align_to(totaldist, direction=LEFT)
-        # self.play(*[ReplacementTransform(totaldist[i], distresult_intermediate[i]) for i in range(4)])
-        # self.wait()
-        # self.play(ReplacementTransform(distresult_intermediate[1:4], distresult_final[1]))
-
-        # self.wait()
-
-# class Q1_Equations(Scene):
-#     def construct(self):
