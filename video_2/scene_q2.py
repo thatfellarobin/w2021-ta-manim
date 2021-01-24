f905257d94b68a7c8b635408fd6cba807c9c7be4
RobinL <Robin.Liu.831@gmail.com>
2021-01-24 14:59:02 -0500

    add magnitude accel solution

--- video_2/scene_q2.py
+++ video_2/scene_q2.py
@@ -40,12 +40,12 @@ class P2(GraphScene):
         theta_arc = Arc(start_angle=0, angle=-26.6*(PI/180), radius=1.5, buff=0, color=YELLOW).move_arc_center_to(self.coords_to_point(200, 100)).add_tip(tip_length=0.2)
 
         # Text
-        y = MathTex('y(x)', '=', '\\frac{k}{x}', color=BLUE).scale(0.8).shift(2.75*RIGHT + 3*UP)
-        y_prime = MathTex('y^{\\prime}(x)', '=', '\\frac{d}{dx}y(x)', '=', '-\\frac{k}{x^2}', color=RED).scale(0.8).next_to(y, DOWN, aligned_edge=LEFT)
+        y = MathTex('y(x)', '=', '\\frac{k}{x}', color=BLUE).scale(0.75).shift(2.75*RIGHT + 3*UP)
+        y_prime = MathTex('y^{\\prime}(x)', '=', '\\frac{d}{dx}y(x)', '=', '-\\frac{k}{x^2}', color=RED).scale(0.75).next_to(y, DOWN, aligned_edge=LEFT)
         theta = MathTex('\\theta_1', '=', '\\mathrm{atan}(y^{\\prime}(x_1))', '=', '-26.6^{\\circ}', color=YELLOW).scale(0.7).next_to(theta_arc, RIGHT)
-        a_cent = MathTex('a_{1c}=', '\\frac{v^2}{\\rho_1}', color=PURPLE).scale(0.8).next_to(a_cent_arrow, direction=a_cent_arrow.get_unit_vector(), buff=0)
-        rho = MathTex('\\rho(x)', '=', '\\frac{\\sqrt{\\left(1+y^{\\prime}(x)^2\\right)^3}}{y^{\\prime\\prime}(x)}').scale(0.8).next_to(y_prime, DOWN, aligned_edge=LEFT)
-        y_double_prime = MathTex('y^{\\prime\\prime}(x)', '=', '\\frac{d}{dx}y^{\\prime}(x)', '=', '\\frac{2k}{x^3}').scale(0.8).next_to(rho, DOWN, aligned_edge=LEFT)
+        a_cent = MathTex('a_{1c}=', '\\frac{v^2}{\\rho_1}', color=PURPLE).scale(0.75).next_to(a_cent_arrow, direction=a_cent_arrow.get_unit_vector(), buff=0)
+        rho = MathTex('\\rho(x)', '=', '\\frac{\\sqrt{\\left(1+y^{\\prime}(x)^2\\right)^3}}{y^{\\prime\\prime}(x)}').scale(0.75).next_to(y_prime, DOWN, aligned_edge=LEFT)
+        y_double_prime = MathTex('y^{\\prime\\prime}(x)', '=', '\\frac{d}{dx}y^{\\prime}(x)', '=', '\\frac{2k}{x^3}').scale(0.75).next_to(rho, DOWN, aligned_edge=LEFT)
 
         a_solution = MathTex(
             '\\vec{a_1}',
@@ -55,7 +55,7 @@ class P2(GraphScene):
             '=',
             r'\begin{bmatrix} 144 \\ 288 \end{bmatrix}',
             '\\mathrm{mm/s^2}')
-        a_solution.scale(0.8).next_to(y_double_prime, DOWN, aligned_edge=LEFT)
+        a_solution.scale(0.75).next_to(y_double_prime, DOWN, aligned_edge=LEFT)
         a_solution[4:].next_to(a_solution[1:4], DOWN, aligned_edge=LEFT)
 
         self.add(path)
@@ -102,8 +102,21 @@ class P2(GraphScene):
         )
         self.wait()
         a_group = Group(a_solution[0:2], a_solution[5:])
+
+        # Magnitude of accel
+        a_solution_magnitude = MathTex(
+            '|\\vec{a_1}|',
+            '=',
+            '\\sqrt{a_{1x}^2 + a_{1y}^2}',
+            '=',
+            '322\\,\\mathrm{mm/s^2}').scale(0.7).next_to(a_group, DOWN, aligned_edge=LEFT)
+        self.play(Write(a_solution_magnitude))
         a_sol_box = SurroundingRectangle(a_group, buff=.1)
         self.play(ShowCreationThenDestruction(a_sol_box))
+        self.wait()
+        a_sol_mag_box = SurroundingRectangle(a_solution_magnitude, buff=.1)
+        self.play(ShowCreationThenDestruction(a_sol_mag_box))
+
         self.wait()
 
 
