3c32f929e762a71a277039233864574a28c6237d
Robin Liu <Robin.Liu.831@gmail.com>
2021-01-29 10:50:35 -0500

    finished part a free body diagrams

--- video_3/scene_q1.py
+++ video_3/scene_q1.py
@@ -6,7 +6,7 @@ GREEN_DARK = '#2b4022'
 
 class PartA(Scene):
     def construct(self):
-        # Scene imagery
+        ## Diagram imagery
         block_A = Rectangle(
             color=TEAL,
             height=1.5,
@@ -54,15 +54,142 @@ class PartA(Scene):
             start=ORIGIN,
             end=2*LEFT
         ).next_to(A, LEFT, buff=0)
-        force_label = MathTex("P", color=RED).scale(1.4).next_to(pull_force, LEFT)
+        force_label = MathTex("P", color=pull_force.get_color()).scale(1.4).next_to(pull_force, LEFT)
 
         diagram = Group(floor, wall, rope, rope_tie_1, rope_tie_2, A, B, pull_force, force_label).move_to(ORIGIN)
 
-
+        # Need to copy before the diagram transformation to preserve the scale
+        A_fbd = A.copy().move_to(ORIGIN).scale(0.55).shift(3.5*LEFT+1.5*UP)
+        B_fbd = B.copy().move_to(ORIGIN).scale(0.55).shift(3.5*RIGHT+1.5*UP)
 
         self.add(diagram)
         self.wait()
 
+        dividing_line = Line(start=3*DOWN, end=3*UP, color=GREY)
+
+        self.play(Transform(diagram, diagram.copy().scale(0.3).shift(5*LEFT+3*UP)))
+        self.play(ShowCreation(dividing_line))
+        self.wait()
+
+        # Animate free body diagram for block A
+        self.play(FadeIn(A_fbd))
+        self.wait()
+        P = Arrow(
+            color=RED,
+            buff=0,
+            start=ORIGIN,
+            end=1.5*LEFT,
+            max_stroke_width_to_length_ratio=999,
+            max_tip_length_to_length_ratio=1
+        ).next_to(A_fbd, LEFT, buff=0)
+        P_label = MathTex("P", color=P.get_color()).scale(0.8).next_to(P, LEFT)
+        A_mg = Arrow(
+            color=BLUE,
+            buff=0,
+            start=ORIGIN,
+            end=1.0*DOWN,
+            max_stroke_width_to_length_ratio=999,
+            max_tip_length_to_length_ratio=1
+        ).shift(A_fbd.get_center()+0.1*LEFT)
+        A_mg_label = MathTex("mg", color=A_mg.get_color()).scale(0.8).next_to(A_mg, LEFT, aligned_edge=DOWN)
+        normal_A_1 = Arrow(
+            color=PURPLE,
+            buff=0,
+            start=ORIGIN,
+            end=0.8*UP,
+            max_stroke_width_to_length_ratio=999,
+            max_tip_length_to_length_ratio=1
+        ).next_to(A_fbd, DOWN, buff=0).shift(0.1*RIGHT)
+        normal_A_1_label = MathTex("N_1", color=normal_A_1.get_color()).scale(0.8).next_to(normal_A_1, RIGHT, buff=0, aligned_edge=DOWN)
+        normal_A_2 = Arrow(
+            color=PURPLE,
+            buff=0,
+            start=ORIGIN,
+            end=0.8*DOWN,
+            max_stroke_width_to_length_ratio=999,
+            max_tip_length_to_length_ratio=1
+        ).next_to(A_fbd, UP, buff=0)
+        normal_A_2_label = MathTex("N_2", color=normal_A_2.get_color()).scale(0.8).next_to(normal_A_2, RIGHT, buff=0, aligned_edge=UP)
+        fric_A_1 = Arrow(
+            color=MAROON,
+            buff=0,
+            start=ORIGIN,
+            end=1.5*RIGHT,
+            max_stroke_width_to_length_ratio=999,
+            max_tip_length_to_length_ratio=1
+        ).shift(A_fbd.get_edge_center(DOWN))
+        fric_A_1_label = MathTex("F_{f1}", color=fric_A_1.get_color()).scale(0.8).next_to(fric_A_1, RIGHT)
+        fric_A_2 = Arrow(
+            color=MAROON,
+            buff=0,
+            start=ORIGIN,
+            end=1.5*RIGHT,
+            max_stroke_width_to_length_ratio=999,
+            max_tip_length_to_length_ratio=1
+        ).shift(A_fbd.get_edge_center(UP))
+        fric_A_2_label = MathTex("F_{f2}", color=fric_A_2.get_color()).scale(0.8).next_to(fric_A_2, RIGHT)
+
+        self.play(Write(P), Write(P_label))
+        self.wait()
+        self.play(Write(A_mg), Write(A_mg_label))
+        self.wait()
+        self.play(Write(normal_A_1), Write(normal_A_1_label))
+        self.wait()
+        self.play(Write(normal_A_2), Write(normal_A_2_label))
+        self.wait()
+        self.play(Write(fric_A_1), Write(fric_A_1_label))
+        self.wait()
+        self.play(Write(fric_A_2), Write(fric_A_2_label))
+        self.wait()
+
+        # Animate free body diagram for block B
+        self.play(FadeIn(B_fbd))
+        self.wait()
+        B_mg = Arrow(
+            color=BLUE,
+            buff=0,
+            start=ORIGIN,
+            end=1.0*DOWN,
+            max_stroke_width_to_length_ratio=999,
+            max_tip_length_to_length_ratio=1
+        ).shift(B_fbd.get_center()+0.1*LEFT)
+        B_mg_label = MathTex("mg", color=B_mg.get_color()).scale(0.8).next_to(B_mg, LEFT, aligned_edge=DOWN)
+        normal_B_2 = Arrow(
+            color=PURPLE,
+            buff=0,
+            start=ORIGIN,
+            end=0.8*UP,
+            max_stroke_width_to_length_ratio=999,
+            max_tip_length_to_length_ratio=1
+        ).next_to(B_fbd, DOWN, buff=0).shift(0.1*RIGHT)
+        normal_B_2_label = MathTex("N_2", color=normal_B_2.get_color()).scale(0.8).next_to(normal_B_2, RIGHT, buff=0, aligned_edge=DOWN)
+        fric_B_2 = Arrow(
+            color=MAROON,
+            buff=0,
+            start=ORIGIN,
+            end=1.5*LEFT,
+            max_stroke_width_to_length_ratio=999,
+            max_tip_length_to_length_ratio=1
+        ).shift(B_fbd.get_edge_center(DOWN))
+        fric_B_2_label = MathTex("F_{f2}", color=fric_B_2.get_color()).scale(0.8).next_to(fric_B_2, LEFT)
+        T = Arrow(
+            color=RED,
+            buff=0,
+            start=ORIGIN,
+            end=1.5*RIGHT,
+            max_stroke_width_to_length_ratio=999,
+            max_tip_length_to_length_ratio=1
+        ).next_to(B_fbd, RIGHT, buff=0)
+        T_label = MathTex("T", color=T.get_color()).scale(0.8).next_to(T, RIGHT)
+
+        self.play(Write(B_mg), Write(B_mg_label))
+        self.wait()
+        self.play(Write(normal_B_2), Write(normal_B_2_label))
+        self.wait()
+        self.play(Write(fric_B_2), Write(fric_B_2_label))
+        self.wait()
+        self.play(Write(T), Write(T_label))
+        self.wait()
 
 class PartB(Scene):
     def construct(self):
