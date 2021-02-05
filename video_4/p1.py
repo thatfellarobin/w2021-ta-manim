5a3f187c32a42b43d6e3b6f17af26c887b8388e5
RobinL <Robin.Liu.831@gmail.com>
2021-02-05 10:39:47 -0500

    finished fbd

--- video_4/p1.py
+++ video_4/p1.py
@@ -222,22 +222,7 @@ class P1(Scene):
 
         #endregion
 
-        diagram_fullyAnnot = Group(
-            diagram_stringAnnot,
-            text_A,
-            text_B,
-            angref_A,
-            angref_B,
-            ang_A,
-            ang_B,
-            ang_A_annot,
-            ang_B_annot,
-            s_A_annot,
-            s_B_annot
-        )
-        string_sum_grouped = Group(string_sum, string_sum_d)
-
-        # At this point, should explain how to identify energy problem?
+        # TODO:, should explain how to identify energy problem?
 
         #region Animate the motion d
         base_string_1 = string_1.get_end()
@@ -312,10 +297,214 @@ class P1(Scene):
         s_B_arrow.clear_updaters()
         #endregion
 
+        #region Annotate d
+        d_arrow = Arrow(
+            start=block_B.get_center() + block_halfwidth*np.array([np.cos(150*(PI/180)), np.sin(150*(PI/180)), 0]),
+            end=block_B.get_center() + 4*block_halfwidth*np.array([np.cos(150*(PI/180)), np.sin(150*(PI/180)), 0]),
+            color=PURPLE,
+            buff=0.0,
+            stroke_width=8,
+            tip_length=0.35,
+            max_stroke_width_to_length_ratio=999,
+            max_tip_length_to_length_ratio=1
+        )
+        d_half_arrow = d_arrow.copy()
+        d_half_arrow.scale_about_point(point=d_arrow.get_start(), scale_factor=0.5).rotate(about_point=d_arrow.get_start(), angle=PI/2)
+        d_half_arrow.shift(block_A.get_center() + block_halfwidth*np.array([np.cos(240*(PI/180)), np.sin(240*(PI/180)), 0]) - d_arrow.get_start())
+        d_arrow_annot = MathTex('d', color=PURPLE).scale(0.9).next_to(d_arrow.get_center(), direction=np.array([np.cos(240*(PI/180)), np.sin(240*(PI/180)), 0]), buff=0.1)
+        d_half_arrow_annot = MathTex('d/2', color=PURPLE).scale(0.9).next_to(d_half_arrow, direction=LEFT, buff=0.1)
+
+        self.play(Write(d_arrow), Write(d_arrow_annot))
+        self.wait()
+        self.play(Write(d_half_arrow), Write(d_half_arrow_annot))
+        self.wait()
+        #endregion
+
         # Prepare to do math
+        diagram_fullyAnnot = Group(
+            diagram_stringAnnot,
+            text_A,
+            text_B,
+            angref_A,
+            angref_B,
+            ang_A,
+            ang_B,
+            ang_A_annot,
+            ang_B_annot,
+            s_A_annot,
+            s_B_annot,
+            d_arrow,
+            d_arrow_annot,
+            d_half_arrow,
+            d_half_arrow_annot
+        )
+        string_sum_grouped = Group(string_sum, string_sum_d)
         self.play(
-            Transform(diagram_fullyAnnot, diagram_fullyAnnot.copy().scale(0.8).shift(3.5*LEFT+1*UP)),
-            Transform(string_sum_grouped, string_sum_grouped.copy().scale(0.8).to_edge(LEFT).shift(0.5*RIGHT+5*DOWN))
+            Transform(diagram_fullyAnnot, diagram_fullyAnnot.copy().scale(0.6).shift(4*LEFT+1.5*UP)),
+            Transform(string_sum_grouped, string_sum_grouped.copy().scale(0.8).to_edge(LEFT).shift(0.5*RIGHT+3.5*DOWN))
         )
         self.wait()
 
+        #region explain different energy components:
+        E_g = MathTex('E_g=mg\\Delta h', color=YELLOW).scale(0.7)
+        E_k = MathTex('E_k=\\frac{1}{2}mv^2', color=YELLOW).scale(0.7)
+        Work = MathTex('Work=Fs', color=YELLOW).scale(0.7)
+        energy_primatives = VGroup(E_g, E_k, Work).arrange(buff=0.5).next_to(diagram_fullyAnnot, RIGHT, aligned_edge=UP, buff=0.75)
+        self.play(Write(energy_primatives))
+        self.wait()
+        hlbox = SurroundingRectangle(E_g, buff=0.15)
+        self.play(ShowCreation(hlbox))
+        self.wait()
+        self.play(Transform(hlbox, SurroundingRectangle(E_k, buff=0.15)))
+        self.wait()
+        self.play(Transform(hlbox, SurroundingRectangle(Work, buff=0.15)))
+        self.wait()
+        self.play(FadeOut(hlbox))
+        self.wait()
+        #endregion
+
+        #region Write energy equation
+        energycons_0 = MathTex('Work_{in} - Work_{out} = \\Delta E_g + \\Delta E_k').scale(0.7).next_to(energy_primatives, DOWN, aligned_edge=LEFT)
+        self.play(Write(energycons_0))
+        self.wait()
+        energycons_1 = MathTex(
+            '-F_{fA}\\frac{d}{2}-F_{fB}d',
+            '=',
+            'W_Bd\\sin(\\theta_2) - W_A\\frac{d}{2}\\sin(\\theta_1)',
+            '+',
+            '\\frac{1}{2}\\frac{W_A}{g}v_A^2 + \\frac{1}{2}\\frac{W_B}{g}v_B^2'
+        ).scale(0.6).next_to(energycons_0, DOWN, aligned_edge=LEFT)
+        energycons_1[3:].next_to(energycons_1[2], DOWN, aligned_edge=LEFT)
+        self.play(Write(energycons_1))
+        self.wait()
+        energycons_1_simp = MathTex(
+            '-\\mu_kN_A\\frac{d}{2} - \\mu_kN_Bd',
+            '=',
+            'W_Bd\\sin(\\theta_2) - W_A\\frac{d}{2}\\sin(\\theta_1)',
+            '+',
+            '\\frac{1}{2g}(W_Av_A^2 + W_Bv_B^2)',
+        ).scale(0.6).next_to(energycons_0, DOWN, aligned_edge=LEFT)
+        energycons_1_simp[3:].next_to(energycons_1_simp[2], DOWN, aligned_edge=LEFT)
+        self.play(*[Transform(energycons_1[i], energycons_1_simp[i]) for i in range(len(energycons_1))])
+        self.wait()
+        #endregion
+
+        #region FBD to solve for N_A and N_B
+        block_halfwidth *= 0.6 # account for scaling operation of the full diagram
+        block_A_fbd = block_A.copy().move_to(0.5*RIGHT+1.5*DOWN)
+        block_A_fbd_label = text_A.copy().move_to(block_A_fbd.get_center())
+        block_B_fbd = block_B.copy().next_to(block_A_fbd, RIGHT, buff=3)
+        block_B_fbd_label = text_B.copy().move_to(block_B_fbd.get_center())
+        fbd_NA = Arrow(
+            start=block_A_fbd.get_center() + 3*block_halfwidth*np.array([np.cos(-30*(PI/180)), np.sin(-30*(PI/180)), 0]),
+            end=block_A_fbd.get_center() + block_halfwidth*np.array([np.cos(-30*(PI/180)), np.sin(-30*(PI/180)), 0]),
+            color=PURPLE,
+            buff=0.0,
+            stroke_width=5,
+            tip_length=0.2,
+            max_stroke_width_to_length_ratio=999,
+            max_tip_length_to_length_ratio=1
+        )
+        fbd_NA_label = MathTex('N_A', color=PURPLE).scale(0.6).next_to(fbd_NA.get_start(), RIGHT, buff=0.1)
+        fbd_NB = Arrow(
+            start=block_B_fbd.get_center() + 3*block_halfwidth*np.array([np.cos(-120*(PI/180)), np.sin(-120*(PI/180)), 0]),
+            end=block_B_fbd.get_center() + block_halfwidth*np.array([np.cos(-120*(PI/180)), np.sin(-120*(PI/180)), 0]),
+            color=PURPLE,
+            buff=0.0,
+            stroke_width=5,
+            tip_length=0.2,
+            max_stroke_width_to_length_ratio=999,
+            max_tip_length_to_length_ratio=1
+        )
+        fbd_NB_label = MathTex('N_B', color=PURPLE).scale(0.6).next_to(fbd_NB.get_start(), LEFT, buff=0.1)
+        fbd_WA = Arrow(
+            start=block_A_fbd.get_center(),
+            end=block_A_fbd.get_center() + 3*block_halfwidth*DOWN,
+            color=BLUE,
+            buff=0.0,
+            stroke_width=5,
+            tip_length=0.2,
+            max_stroke_width_to_length_ratio=999,
+            max_tip_length_to_length_ratio=1
+        )
+        fbd_WA_label = MathTex('W_A', color=BLUE).scale(0.6).next_to(fbd_WA.get_end(), DOWN, buff=0.1)
+        fbd_WB = Arrow(
+            start=block_B_fbd.get_center(),
+            end=block_B_fbd.get_center() + 3*block_halfwidth*DOWN,
+            color=BLUE,
+            buff=0.0,
+            stroke_width=5,
+            tip_length=0.2,
+            max_stroke_width_to_length_ratio=999,
+            max_tip_length_to_length_ratio=1
+        )
+        fbd_WB_label = MathTex('W_B', color=BLUE).scale(0.6).next_to(fbd_WB.get_end(), DOWN, buff=0.1)
+        fbd_TA = Arrow(
+            start=block_A_fbd.get_center() + block_halfwidth*np.array([np.cos(60*(PI/180)), np.sin(60*(PI/180)), 0]),
+            end=block_A_fbd.get_center() + 3*block_halfwidth*np.array([np.cos(60*(PI/180)), np.sin(60*(PI/180)), 0]),
+            color=RED,
+            buff=0.0,
+            stroke_width=5,
+            tip_length=0.2,
+            max_stroke_width_to_length_ratio=999,
+            max_tip_length_to_length_ratio=1
+        )
+        fbd_TA_label = MathTex('2T', color=RED).scale(0.6).next_to(fbd_TA.get_end(), RIGHT, buff=0.1)
+        fbd_TB = Arrow(
+            start=block_B_fbd.get_center() + block_halfwidth*np.array([np.cos(150*(PI/180)), np.sin(150*(PI/180)), 0]),
+            end=block_B_fbd.get_center() + 3*block_halfwidth*np.array([np.cos(150*(PI/180)), np.sin(150*(PI/180)), 0]),
+            color=RED,
+            buff=0.0,
+            stroke_width=5,
+            tip_length=0.2,
+            max_stroke_width_to_length_ratio=999,
+            max_tip_length_to_length_ratio=1
+        )
+        fbd_TB_label = MathTex('T', color=RED).scale(0.6).next_to(fbd_TB.get_end(), LEFT, buff=0.1)
+        fbd_FA = Arrow(
+            start=block_A_fbd.get_center() + block_halfwidth*np.array([np.cos(-30*(PI/180)), np.sin(-30*(PI/180)), 0]),
+            end=block_A_fbd.get_center() + block_halfwidth*np.array([np.cos(-30*(PI/180)), np.sin(-30*(PI/180)), 0]) + 2*block_halfwidth*np.array([np.cos(60*(PI/180)), np.sin(60*(PI/180)), 0]),
+            color=MAROON,
+            buff=0.0,
+            stroke_width=5,
+            tip_length=0.2,
+            max_stroke_width_to_length_ratio=999,
+            max_tip_length_to_length_ratio=1
+        )
+        fbd_FA_label = MathTex('F_{fA}', color=MAROON).scale(0.6).next_to(fbd_FA.get_end(), RIGHT, buff=0.1)
+        fbd_FB = Arrow(
+            start=block_B_fbd.get_center() + block_halfwidth*np.array([np.cos(-120*(PI/180)), np.sin(-120*(PI/180)), 0]),
+            end=block_B_fbd.get_center() + block_halfwidth*np.array([np.cos(-120*(PI/180)), np.sin(-120*(PI/180)), 0]) + 2*block_halfwidth*np.array([np.cos(-30*(PI/180)), np.sin(-30*(PI/180)), 0]),
+            color=MAROON,
+            buff=0.0,
+            stroke_width=5,
+            tip_length=0.2,
+            max_stroke_width_to_length_ratio=999,
+            max_tip_length_to_length_ratio=1
+        )
+        fbd_FB_label = MathTex('F_{fB}', color=MAROON).scale(0.6).next_to(fbd_FB.get_end(), RIGHT, buff=0.1)
+
+        self.play(
+            FadeIn(block_A_fbd),
+            FadeIn(block_A_fbd_label),
+            FadeIn(block_B_fbd),
+            FadeIn(block_B_fbd_label),
+            FadeIn(fbd_NA),
+            FadeIn(fbd_NA_label),
+            FadeIn(fbd_NB),
+            FadeIn(fbd_NB_label),
+            FadeIn(fbd_WA),
+            FadeIn(fbd_WA_label),
+            FadeIn(fbd_WB),
+            FadeIn(fbd_WB_label),
+            FadeIn(fbd_TA),
+            FadeIn(fbd_TA_label),
+            FadeIn(fbd_TB),
+            FadeIn(fbd_TB_label),
+            FadeIn(fbd_FA),
+            FadeIn(fbd_FA_label),
+            FadeIn(fbd_FB),
+            FadeIn(fbd_FB_label)
+        )
+        self.wait()
+        #endregion
