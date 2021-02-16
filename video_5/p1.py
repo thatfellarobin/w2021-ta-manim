7d2c12f6561b14d33000996093647b613d46d0b2
Robin Liu <Robin.Liu.831@gmail.com>
2021-02-16 12:05:18 -0500

    handoff to home pc

--- video_5/p1.py
+++ video_5/p1.py
@@ -61,17 +61,118 @@ class T5P1(Scene):
         diagram_block.shift(3.5*LEFT)
 
         diagram_full = Group(ground, trees, diagram_block)
-        diagram_full.shift(2.5*DOWN)
+        diagram_full.shift(2*DOWN)
 
         self.add(diagram_full)
         self.wait()
         #endregion
 
-        #region From A's perspective
+        # copy some stuff for later
+        block_initpos = block.copy().set_opacity(0.3)
+        diagram_block_B = diagram_block.copy()
+        trees_B = trees.copy()
 
+        #region From A's perspective
+        path_A = Line(
+            start=ORIGIN,
+            end=7.5*RIGHT
+        ).set_opacity(0).shift(diagram_block.get_center())
+        d_arrow = DoubleArrow(
+            start=ORIGIN,
+            end=7.5*RIGHT,
+            color=BLUE,
+            buff=0.0,
+            stroke_width=8,
+            tip_length=0.3,
+            max_stroke_width_to_length_ratio=999,
+            max_tip_length_to_length_ratio=1
+        ).shift(block.get_edge_center(LEFT)).shift(DOWN)
+        d_label = MathTex('d', color=BLUE).scale(0.8).next_to(d_arrow, DOWN)
+
+        self.play(MoveAlongPath(diagram_block, path_A, rate_func=rate_functions.ease_in_quad, run_time=2))
+        self.play(FadeIn(block_initpos))
+        self.wait()
+        self.play(
+            FadeIn(d_arrow),
+            Write(d_label)
+        )
+        self.wait()
+
+        energy_0 = MathTex(
+            '\\Delta W',
+            '=',
+            '\\Delta E_g',
+            '+',
+            '\\Delta E_k'
+        ).scale(0.9).to_edge(UP, buff=1)
+        energy_1 = MathTex(
+            'Fd',
+            '=',
+            '\\frac{1}{2}m(v_{1A}^2-v_0^2)'
+        ).scale(0.9)
+        energy_1.shift(energy_0[1].get_center()-energy_1[1].get_center())
+        self.play(Write(energy_0))
+        self.wait()
+        self.play(ReplacementTransform(energy_0[:2], energy_1[:2]))
+        self.wait()
+        self.play(FadeOut(energy_0[2:4]))
+        self.play(ReplacementTransform(energy_0[4], energy_1[2]))
+        self.wait()
+        v1A = MathTex('v_{1A} = 6.08\\,\\mathrm{m/s}').scale(0.9).shift(UP)
+        hlbox_v1A = SurroundingRectangle(v1A, buff=0.15)
+        self.play(Write(v1A))
+        self.play(ShowCreation(hlbox_v1A))
+        # cleanup
+        v1A_group = Group(v1A, hlbox_v1A)
+        self.wait()
+        self.play(
+            FadeOut(energy_1),
+            Transform(v1A_group, v1A_group.copy().to_corner(UP+RIGHT), buff=0.75)
+        )
+        self.wait()
+        #endregion
+
+        #region time of travel
+        distance_eq = MathTex(
+            'd',
+            '=',
+            'v_0t',
+            '+',
+            '\\frac{1}{2}at^2',
+        ).scale(0.8).to_corner(UP+LEFT, buff=1)
+        distance_eq_subbed = MathTex(
+            'd',
+            '=',
+            'v_0t',
+            '+',
+            '\\frac{1}{2}\\frac{F}{M}t^2',
+            '\\Rightarrow',
+            't = 1.80\\,\\mathrm{s}'
+        ).scale(0.8)
+        distance_eq_subbed.shift(distance_eq[1].get_center()-distance_eq_subbed[1].get_center())
+        self.play(Write(distance_eq))
+        self.wait()
+        self.play(*[ReplacementTransform(distance_eq[i], distance_eq_subbed[i]) for i in range(len(distance_eq))])
+        self.wait()
+        self.play(Write(distance_eq_subbed[-2:]))
+        hlbox_t = SurroundingRectangle(distance_eq_subbed[-1], buff=0.15)
+        self.play(ShowCreation(hlbox_t))
+        self.wait()
+        # cleanup
+        self.play(FadeOut(distance_eq_subbed[:-1]))
+        t_group = Group(distance_eq_subbed[-1], hlbox_t)
+        self.play(Transform(t_group, t_group.copy().next_to(v1A_group, DOWN, aligned_edge=RIGHT)))
+        self.wait()
         #endregion
 
         #region From B's perspective
-
+        self.play(
+            FadeOut(block_initpos),
+            FadeOut(d_arrow),
+            FadeOut(d_label)
+        )
         #endregion
+
+
+
 
