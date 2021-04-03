106ff1625f5fe6f361f30402542289c6ecddfa89
RobinL <Robin.Liu.831@gmail.com>
2021-04-03 11:34:14 -0400

    finished prelim diagram on p1

--- video_11/p1.py
+++ video_11/p1.py
@@ -11,7 +11,7 @@ BLUE_E_DARK = '#0c343d'
 
 DSCALE = 2
 
-class T10P3(Scene):
+class T11P1(Scene):
     def number_equation(self, eq, n, color=YELLOW_B):
         num = MathTex('\\textbf{' + str(n) + '}', color=color).scale(0.5)
         circle = Circle(
@@ -42,187 +42,181 @@ class T10P3(Scene):
         self.add(attribution)
 
         #region Diagram objects
-        pulley_1 = Circle(
-            radius=0.5,
+        def create_A_frame():
+            left = Line(
+                start=0.4*LEFT+1.3*DOWN,
+                end=ORIGIN,
+                color=BROWN,
+                stroke_width=12
+            )
+            right = left.copy().flip(axis=UP)
+            right.shift(left.get_end()-right.get_end())
+            horiz = Line(
+                start=left.get_center(),
+                end=right.get_center(),
+                color=BROWN,
+                stroke_width=12
+            )
+            pivot = Dot(
+                point=left.get_end(),
+                color=BROWN,
+                radius=0.12
+            )
+            Aframe = Group(left, right, horiz, pivot)
+            return Aframe
+
+        ground = Line(
+            start=3*LEFT,
+            end=3*RIGHT,
+            color=GREY
+        )
+
+        A_frame_left = create_A_frame().shift(DSCALE*0.5*LEFT).align_to(ground, DOWN)
+        A_frame_right = create_A_frame().shift(DSCALE*0.5*RIGHT).align_to(ground, DOWN)
+
+        beam = Rectangle(
+            height=0.125,
+            width=DSCALE*2*(0.9+0.5),
             color=BLUE_E,
             fill_color=BLUE_E_DARK,
-            fill_opacity=1,
-            stroke_width=10
-        ).shift(2*UP)
-        pulley_2 = Dot(
-            color=BLUE_E
-        ).move_to(pulley_1.get_center())
-        pulley = Group(pulley_1, pulley_2)
-
-        string_left = Line(
-            start=pulley.get_edge_center(LEFT),
-            end=pulley.get_edge_center(LEFT)+DSCALE*1*DOWN,
-            color=YELLOW_B,
-        )
-        string_right = Line(
-            start=pulley.get_edge_center(RIGHT),
-            end=pulley.get_edge_center(RIGHT)+DSCALE*2*DOWN
-        )
-        string_right.set_color([BLACK, YELLOW_B, YELLOW_B])
-
-        block = Rectangle(
-            width=0.5,
-            height=0.75,
-            color=BLUE,
-            fill_color=BLUE_DARK,
             fill_opacity=1
-        ).next_to(string_left, DOWN, buff=0)
+        ).next_to(Group(A_frame_left, A_frame_right), UP, buff=0)
 
         diagram = Group(
-            string_left, string_right, pulley, block
-        )
-
+            ground,
+            A_frame_left,
+            A_frame_right,
+            beam
+        ).move_to(ORIGIN)
         self.add(diagram)
         self.wait()
         #endregion
 
         #region Annotate diagram
-        s_base = Line(
-            start=pulley.get_edge_center(LEFT),
-            end=pulley.get_edge_center(LEFT)+0.4*LEFT,
-            color=GREEN
-        ).shift(0.2*LEFT)
-        s_arrow = Arrow(
-            start=s_base.get_center(),
-            end=np.array([s_base.get_center()[0], block.get_edge_center(UP)[1], 0]),
-            color=GREEN,
-            buff=0.0,
-            stroke_width=5,
-            tip_length=0.15,
-            max_stroke_width_to_length_ratio=999,
-            max_tip_length_to_length_ratio=1
+        point_A = Dot(
+            point=A_frame_left.get_edge_center(UP),
+            radius=0.05,
+            color=YELLOW
+        ).shift((0.125/2)*UP) # shift up half of beams height. tried to use .align_to(), but it's broken >:(
+        point_A_label = MathTex('A', color=YELLOW).scale(0.6).next_to(point_A, DOWN, buff=0.15)
+        point_B = Dot(
+            point=A_frame_right.get_edge_center(UP),
+            radius=0.05,
+            color=YELLOW
+        ).shift((0.125/2)*UP) # shift up half of beams height. tried to use .align_to(), but it's broken >:(
+        point_B_label = MathTex('B', color=YELLOW).scale(0.6).next_to(point_B, DOWN, buff=0.15)
+        point_C = Dot(
+            point=beam.get_edge_center(LEFT),
+            radius=0.05,
+            color=YELLOW
         )
-        s_label = MathTex('s', color=GREEN).scale(0.6).next_to(s_arrow, LEFT, buff=0.15)
-        r_arrow = Arrow(
-            start=pulley.get_center(),
-            end=pulley_1.point_at_angle(PI/4),
+        point_C_label = MathTex('C', color=YELLOW).scale(0.6).next_to(point_C, LEFT, buff=0.15)
+        point_D = Dot(
+            point=beam.get_edge_center(RIGHT),
+            radius=0.05,
+            color=YELLOW
+        )
+        point_D_label = MathTex('D', color=YELLOW).scale(0.6).next_to(point_D, RIGHT, buff=0.15)
+        point_G = Dot(
+            point=beam.get_center(),
+            radius=0.05,
+            color=YELLOW
+        )
+        point_G_label = MathTex('G', color=YELLOW).scale(0.6).next_to(point_G, UP, buff=0.15)
+
+        b_arrow_left = DoubleArrow(
+            start=point_A.get_center(),
+            end=point_G.get_center(),
             color=YELLOW,
             buff=0.0,
             stroke_width=5,
             tip_length=0.15,
             max_stroke_width_to_length_ratio=999,
             max_tip_length_to_length_ratio=1
-        )
-        r_label = MathTex('r', color=YELLOW).scale(0.6).next_to(r_arrow.get_end(), UP+RIGHT, buff=0.15)
-
-        self.play(
-            ShowCreation(s_base),
-            Write(s_arrow),
-            Write(s_label),
-            Write(r_arrow),
-            Write(r_label)
-        )
-        self.wait()
-        diagram = Group(
-            diagram,
-            s_base,
-            s_arrow,
-            s_label,
-            r_arrow,
-            r_label
-        )
-        #endregion
-
-        #region Animate falling
-        self.play(Transform(s_label, MathTex('s_1', color=GREEN).scale(0.6).move_to(s_label.get_center())))
-        self.wait()
-
-        block_faded = block.copy().set_opacity(0)
-        new_string_left = string_left.copy().set_length(DSCALE*2)
-        new_string_left.shift(pulley.get_edge_center(LEFT)-new_string_left.get_start())
-        new_s_base = s_base.copy().scale(2.5)
-        new_s_base.shift(s_base.get_start()-new_s_base.get_start())
-        s2_arrow = Arrow(
-            start=new_s_base.get_end()+0.2*RIGHT,
-            end=np.array([(new_s_base.get_end()+0.2*RIGHT)[0], (block.get_edge_center(UP)+2*DOWN)[1], 0]),
-            color=GREEN,
+        ).shift(1.75*DOWN)
+        b_arrow_label_left = MathTex('b', color=YELLOW).scale(0.6).next_to(b_arrow_left, DOWN, buff=0.1)
+        b_arrow_right = DoubleArrow(
+            start=point_G.get_center(),
+            end=point_B.get_center(),
+            color=YELLOW,
             buff=0.0,
             stroke_width=5,
             tip_length=0.15,
             max_stroke_width_to_length_ratio=999,
             max_tip_length_to_length_ratio=1
-        )
-        s2_label = MathTex('s_2', color=GREEN).scale(0.6).next_to(s2_arrow, LEFT, buff=0.15)
+        ).shift(1.75*DOWN)
+        b_arrow_label_right = MathTex('b', color=YELLOW).scale(0.6).next_to(b_arrow_right, DOWN, buff=0.1)
+
+        a_arrow_left = DoubleArrow(
+            start=point_C.get_center(),
+            end=point_A.get_center(),
+            color=YELLOW,
+            buff=0.0,
+            stroke_width=5,
+            tip_length=0.15,
+            max_stroke_width_to_length_ratio=999,
+            max_tip_length_to_length_ratio=1
+        ).shift(1.75*DOWN)
+        a_arrow_label_left = MathTex('a', color=YELLOW).scale(0.6).next_to(a_arrow_left, DOWN, buff=0.1)
+        a_arrow_right = DoubleArrow(
+            start=point_B.get_center(),
+            end=point_D.get_center(),
+            color=YELLOW,
+            buff=0.0,
+            stroke_width=5,
+            tip_length=0.15,
+            max_stroke_width_to_length_ratio=999,
+            max_tip_length_to_length_ratio=1
+        ).shift(1.75*DOWN)
+        a_arrow_label_right = MathTex('a', color=YELLOW).scale(0.6).next_to(a_arrow_right, DOWN, buff=0.1)
 
         self.play(
-            Transform(block_faded, block_faded.copy().set_opacity(0.25), run_time=2),
-            Transform(string_left, new_string_left, run_time=2),
-            Transform(block, block.copy().shift(DSCALE*1*DOWN), run_time=2),
-            Transform(s_base, new_s_base, run_time=2)
+            FadeIn(point_A),
+            Write(point_A_label),
+            FadeIn(point_B),
+            Write(point_B_label),
+            FadeIn(point_C),
+            Write(point_C_label),
+            FadeIn(point_D),
+            Write(point_D_label),
+            FadeIn(point_G),
+            Write(point_G_label)
         )
         self.play(
-            Write(s2_arrow),
-            Write(s2_label)
+            Write(b_arrow_left),
+            Write(b_arrow_label_left),
+            Write(b_arrow_right),
+            Write(b_arrow_label_right),
+            Write(a_arrow_left),
+            Write(a_arrow_label_left),
+            Write(a_arrow_right),
+            Write(a_arrow_label_right),
         )
         self.wait()
 
         diagram = Group(
             diagram,
-            block_faded,
-            s2_arrow,
-            s2_label
+            point_A,
+            point_A_label,
+            point_B,
+            point_B_label,
+            point_C,
+            point_C_label,
+            point_D,
+            point_D_label,
+            point_G,
+            point_G_label,
+            b_arrow_left,
+            b_arrow_label_left,
+            b_arrow_right,
+            b_arrow_label_right,
+            a_arrow_left,
+            a_arrow_label_left,
+            a_arrow_right,
+            a_arrow_label_right,
         )
         #endregion
 
-        # Cleanup
-        self.play(Transform(diagram, diagram.copy().scale(0.9).to_corner(DOWN+RIGHT, buff=1)))
-        self.wait()
+        #region Animate the key points in time
 
-        #region Math
-        energy = MathTex(
-            '\\Delta {W}',
-            '=',
-            '\\Delta E_{grav}',
-            '+',
-            '\\Delta E_{kin}',
-        ).scale(0.65).to_corner(UP+LEFT, buff=0.75)
-        energy_0 = MathTex(
-            '0',
-            '=',
-            '-m_Ag(s_2-s_1)',
-            '+',
-            '\\frac{1}{2}m_Av_A^2',
-            '+',
-            '\\frac{1}{2}m_Sk_O^2\\omega_S^2',
-        ).scale(0.65).next_to(energy, DOWN, aligned_edge=LEFT)
-        self.play(Write(energy))
-        self.wait()
-        self.play(
-            Write(energy_0[0])
-        )
-        self.wait(0.5)
-        self.play(
-            Write(energy_0[1:3])
-        )
-        self.wait(0.5)
-        self.play(
-            Write(energy_0[3:5])
-        )
-        self.wait(0.5)
-        self.play(
-            Write(energy_0[5:7])
-        )
-        self.number_equation(energy_0, 1)
-        self.wait()
-
-        kin_eq = MathTex(
-            'v_A = r\\omega_S'
-        ).scale(0.55).next_to(energy_0, DOWN, aligned_edge=LEFT)
-        self.play(Write(kin_eq))
-        self.number_equation(kin_eq, 2)
-        self.wait()
-
-
-        ans = MathTex(
-            '\\omega_S = 41.8\\,\\mathrm{rad/s}'
-        ).scale(0.7).next_to(kin_eq, DOWN, aligned_edge=LEFT, buff=0.5).shift(0.15*RIGHT)
-        ansbox = SurroundingRectangle(ans, buff=0.15)
-        self.play(Write(ans))
-        self.play(ShowCreation(ansbox))
-        self.wait()
         #endregion
