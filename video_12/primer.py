0f53dca43a8d8a248dc156345edfcdeeb4c22f5c
RobinL <Robin.Liu.831@gmail.com>
2021-04-09 12:26:45 -0400

    started vibration primer subvideo

---
+++ video_12/primer.py
@@ -0,0 +1,64 @@
+from manim import *
+import numpy as np
+
+GOLD_DARK = '#5c4326'
+EVERGREEN = '#077319'
+GREEN_DARK = '#2b4022'
+BLUE_DARK = '#26545e'
+BROWN = '#8f4a04'
+MED_DARK_GREY = '#666666'
+BLUE_E_DARK = '#0c343d'
+
+BALL_RADIUS = 0.75
+BALL_HEIGHT = 2
+BALL_OFFSET = 0.5
+
+class VibPrimer(Scene):
+    def number_equation(self, eq, n, color=YELLOW_B):
+        num = MathTex('\\textbf{' + str(n) + '}', color=color).scale(0.5)
+        circle = Circle(
+            color=color,
+            radius=0.225,
+            arc_center=num.get_center()
+        )
+        line = Line(
+            start=circle.get_edge_center(LEFT),
+            end=circle.get_edge_center(LEFT)+0.6*LEFT,
+            color=color
+        )
+        group = Group(num, circle, line)
+        group.next_to(eq, RIGHT, buff=0.25)
+        self.play(FadeIn(group))
+        return group
+
+    def recursive_set_opacity(self, mobj, opacity=0):
+        for item in mobj:
+            if len(item) > 1:
+                self.recursive_set_opacity(mobj=item, opacity=opacity)
+            else:
+                item.set_opacity(opacity)
+        return mobj
+
+    def construct(self):
+        attribution = Tex('Robin Liu, 2021', color=MED_DARK_GREY).scale(0.4).to_corner(DOWN+RIGHT, buff=0.2)
+        self.add(attribution)
+
+        # Step one: use Newton's second law to get the equation of motion
+
+        step1_tex = Tex(
+            '\\textbf{Step 1:}',
+            ' Use Newton\'s second law to get the equation of motion',
+            color=YELLOW
+        ).scale(0.6).to_corner(UP+LEFT, buff=0.5)
+        self.play(Write(step1_tex))
+        self.wait()
+        newton_law = MathTex(
+            '\\Sigma F = m\\ddot{x}',
+            '\\Sigma M = I\\ddot{\\theta}'
+        ).scale(0.5).next_to(step1_tex, DOWN, aligned_edge=LEFT)
+
+        # Step two: Rearrange it so that all the terms are on one side and the acceleration term has a coefficient of 1
+        eq_motion = MathTex(
+            '\\ddot{x} + 2\\zeta \\omega_n\\dot{x} + \\omega_n^2 x = 0',
+            '\\ddot{\\theta} + 2\\zeta \\omega_n\\dot{\\theta} + \\omega_n^2 \\theta = 0'
+        ).scale(0.6)
